66ac62677a03d4c5aa1fb3c48377f228a37b93b3
Federico Rampazzo <frampone@gmail.com>
2014-11-19 01:43:19 +0000

    new cleanup, new mouse tracking

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-var content = document.getElementById('content');
-var smaller = document.getElementById('smaller');
-var bigger = document.getElementById('bigger');
+var $ = document.getElementById.bind(document);
+
+var content = $('content');
+var smaller = $('smaller');
+var bigger = $('bigger');
 
 content.style.fontSize = '400%';
 bigger.addEventListener('click', function(){
@@ -14,87 +16,6 @@ smaller.addEventListener('click', function(){
   return false;
 });
 
-function arcColor(canvas, range, format){
-  if (typeof canvas == 'string')
-    canvas = document.getElementById(canvas);
-  var ctx = canvas.getContext('2d');
-
-  for(var x=0; x<canvas.width; x++){
-    for(var y=0; y<canvas.height; y++){
-      var yc = y-canvas.height;
-      var xc = x-0;
-      var angle = Math.atan2(yc,xc)/Math.PI*720+360;
-      var value = angle/360*range;
-      ctx.fillStyle = format.replace('#', value);
-      ctx.fillRect(x, y, 1, 1);
-    }
-  }
-  return canvas;
-}
-
-function arcEvent(canvas, range, cb){
-  if (typeof canvas == 'string')
-    canvas = document.getElementById(canvas);
-  var options = {
-    canvas: canvas,
-    range: range,
-    mousedown: false
-  };
-  var optionsValid = {
-    canvas: canvas,
-    range: range,
-    mousedown: true
-  }
-  canvas.addEventListener('click', emitColorValue.bind(null, optionsValid, cb), false);
-  canvas.addEventListener('mousedown', startEmitting.bind(null, options, cb), false);
-  canvas.addEventListener('mousemove', emitColorValue.bind(null, options, cb), false);
-  canvas.addEventListener('mouseup', stopEmitting.bind(null, options), false);
-  window.addEventListener('mouseout', stopEmitting.bind(null, options), false);
-  return canvas;
-}
-function emitColorValue(options, cb, evt){
-  var canvas = options.canvas;
-  var range = options.range;
-  if (!options.mousedown)
-    return;
-  console.log(canvas.id);
-
-  var rect = canvas.getBoundingClientRect();
-  var x = Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width);
-  var y = Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height);
-  
-  var yc = y-canvas.height;
-  var xc = x-0;
-  var angle = Math.atan2(yc,xc)/Math.PI*720+360;
-  var value = angle/360*range;
-  cb.call(evt, Math.ceil(value));
-  return true;
-}
-function startEmitting(options, cb, evt){
-  options.mousedown = true;
-  return true;
-}
-function stopEmitting(options){
-  options.mousedown = false;
-  return true;
-}
-function updateComponent(type, component, value){
-  components[type][component] = value;
-  for (var u in updates[component]){
-    var updatedComponent = updates[component][u];
-    var id = type + '-color-picker-' + updatedComponent;
-    arcColor(id, ranges[updatedComponent], formats(type)[updatedComponent]);
-  }
-  var hsl = [ 
-    components[type].hue,
-    components[type].sat + '%',
-    components[type].lig + '%'
-  ];
-  document.getElementById(type + '-current-color').style.background = 'hsl(' + hsl + ')';
-  
-  var element = boundElements[type];
-  document.getElementById(element[0]).style[element[1]] = 'hsl(' + hsl + ')';
-}
 
 var components = {
   background: {
@@ -104,37 +25,22 @@ var components = {
   },
   text: {
     hue: 0,
-    sat: 100,
-    lig: 50
+    sat: 0,
+    lig: 100
   }
 };
-
-function formats(type){
-  return {
-    hue: 'hsl(#,100%,50%)',
-    sat: 'hsl(' + components[type]['hue'] + ',#%,50%)',
-    lig: 'hsl(' + components[type]['hue'] + ',100%,#%)'
-  };
-}
-var ranges = {
-  hue: 360,
-  sat: 100,
-  lig: 100
+var sizes = {
+  hue: [70,100],
+  sat: [100,130],
+  lig: [130,160]
 };
-var updates = {
-  hue: ['sat', 'lig'],
-  sat: [],
-  lig: [],
-}
 var boundElements = {
-  background: ['content', 'background'],
-  text: ['message', 'color']
+  background: [
+    ['content', 'background']
+  ],
+  text: [
+    ['message', 'color']
+  ]
 };
 
-for (var type in components){
-  for (var component in components[type]){
-    var id = type + '-color-picker-' + component;
-    arcColor(id, ranges[component], formats(type)[component]);
-    arcEvent(id, ranges[component], updateComponent.bind(null, type, component));
-  }
-}
+colorpicker(components, sizes, boundElements);
---
+++ js/colorpicker.js
@@ -0,0 +1,105 @@
+function colorpicker(components, sizes, boundElements){
+  
+  function formats(type){
+    return {
+      hue: 'hsl(#,' + components[type]['sat'] + '%,' + components[type]['lig'] + '%)',
+      sat: 'hsl(' + components[type]['hue'] + ',#%,' + components[type]['lig'] + '%)',
+      lig: 'hsl(' + components[type]['hue'] + ',' + components[type]['sat'] + '%,#%)'
+    };
+  }
+
+  var ranges = {
+    hue: 360,
+    sat: 100,
+    lig: 100
+  };
+
+  function arcColor(canvas, range, format){
+    if (typeof canvas == 'string')
+      canvas = $(canvas);
+    var ctx = canvas.getContext('2d');
+
+    for(var x=0; x<canvas.width; x++){
+      for(var y=0; y<canvas.height; y++){
+        var angle = Math.atan2(y-canvas.height,x)/Math.PI*720+360;
+        var value = angle/360*range;
+        ctx.fillStyle = format.replace('#', value);
+        ctx.fillRect(x, y, 1, 1);
+      }
+    }
+    return canvas;
+  }
+  function drawColorPicker(type){
+    for (var component in components[type]){
+      var id = type + '-color-picker-' + component;
+      arcColor(id, ranges[component], formats(type)[component]);
+    }
+    var hsl = [ 
+      components[type].hue,
+      components[type].sat + '%',
+      components[type].lig + '%'
+    ];
+    $(type + '-current-color').style.background = 'hsl(' + hsl + ')';
+    
+    var elements = boundElements[type];
+    for (var i in elements){
+      $(elements[i][0]).style[elements[i][1]] = 'hsl(' + hsl + ')';
+    }
+  }
+
+  function setupEvents(type){
+    var id = type + '-color-picker-input';
+    var input = $(id);
+    
+    var options = {
+      type: type,
+      mousedown: false
+    };
+    var optionsAlways = {
+      type: type,
+      mousedown: true
+    }
+    var emit = handleComponents.bind(input, options, drawColorPicker);
+    var emitAlways = handleComponents.bind(input, optionsAlways, drawColorPicker);
+    var track = startEmitting.bind(null, options)
+    var stopTrack = stopEmitting.bind(null, options)
+    
+    input.addEventListener('click', emitAlways, false);
+    input.addEventListener('mousedown', track, false);
+    input.addEventListener('mousemove', emit, false);
+    input.addEventListener('mouseup', stopTrack, false);
+    window.addEventListener('mouseout', stopTrack, false);
+  }
+  function handleComponents(options, cb, e){
+    if (!options.mousedown)
+      return;
+    var x = e.pageX - this.offsetLeft;
+    var y = e.pageY - this.offsetTop;
+    if (options.type==='text')
+      x = this.clientWidth - x;
+    var dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y-this.clientHeight, 2));
+    var angle = Math.atan2(y-this.clientHeight,x)/Math.PI*720+360;
+    
+    for (var component in sizes){
+      if (dist>=sizes[component][0] && dist<=sizes[component][1]){
+        var value = angle/360*ranges[component];
+        components[options.type][component] = value;
+        if (cb)
+          cb(options.type, component, value);
+        return;
+      }
+    }
+  }
+  function startEmitting(options){
+    options.mousedown = true;
+  }
+  function stopEmitting(options){
+    options.mousedown = false;
+  }
+
+  for (var type in components){
+    drawColorPicker(type);
+    setupEvents(type);
+  }
+
+}
